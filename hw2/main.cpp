65d616bd2ba5d8ed41f3d69ecd4b17c7c2b7e9f8
dan7936 <dan79361@gmail.com>
2021-05-30 00:27:18 +0300

    my second homework true

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -1,18 +1,19 @@
 ﻿#include <iostream>
 
 const int buf_erazer = 35478; //задаем константу для функции cin.ignore
-const int GameField = 3;
+const int GameField = 3; // задаем константу для массива игрового поля в крестики-нолики
 
 int main()
 {
 	
     {
+		std::cout << "----------Task 1----------" << std::endl;
 /*
 Инициализируем переменные 
 указанных типов (short int, int, long long, char, bool, float, double), 
 используя ввод и вывод через консоль.
 */
-		/*
+		
 		//воодим переменную формата 'short int'
 		while (true)
 		{
@@ -167,10 +168,11 @@ int main()
 				break;
 			}
 		}
-		*/
+		
 	}
 	
 	{
+	std::cout << "----------Task 2----------" << std::endl;
 /*
 Создаем перечеслиние (enum), 
 с возможными вариантами символов 
@@ -182,29 +184,106 @@ int main()
 	std::cout << "For definition of ZERO we will using: " << Game_Chars[ZERO] << std::endl;
 	std::cout << "For definition of EMPTY we will using: " << Game_Chars[EMPTY] << std::endl;
 
-	Tic_Tac_Toe Array[GameField][GameField] = { CROSS };                                               
+	std::cout << "----------Task 3----------" << std::endl;
+
+	// !!!!!! Tic_Tac_Toe Array[GameField][GameField] = { CROSS }; вот тут какая то магия происходит у меня, если инициализирую вот так, 
+	// !!!!!! то при смене инициализатора на тот же ZERO, программа меняет на 1 только первую переменную в массиве
+	// !!!!!! по совету использовал следующую инициализацию:
+	Tic_Tac_Toe Array[GameField][GameField];
+	for (int row = 0; row < GameField; ++row)
+		for (int col = 0; col < GameField; ++col)
+			Array[row][col] = EMPTY;
+
+	std::cout << "Our array right now is using EMPTY ' ' inicialisation, so we will see empty space " << std::endl;
 	// мне открыли глаза, что можно использовать число из одного массива, 
 	// как индекс для вытягивание значения из другого массива, вух вот это магия
 	std::cout << "First symbol in Array = " << Game_Chars[Array[0][0]] << " Second symbol in Array = " << Game_Chars[Array[0][1]] << std::endl; 
-	{
+
+	// написал цикл для вывода массива на экран целиком
+	
 		for (int row = 0; row < GameField; ++row)
 		{
 			for (int col = 0; col < GameField; ++col)
-				std::cout << Game_Chars[Array[row][col]] << "\t";
+				std::cout << "|" << Game_Chars[Array[row][col]] << "|   ";
 
 			std::cout << '\n';
 
 		}
+
+		std::cout << "----------Task 4*----------" << std::endl;
+/*
+Пробую создать и инициализировать сложную структуру, 
+не уверен, что вообще правильно понял задание
+*/
+		{
+			enum MatchResult { WIN, LOOSE, DRAW, NOTDECIDED }; // задаем массив для хранения переменных победы или поражения в матче
+			enum GameInProgress {PLAYING, ENDED}; // задаем массив для хранения переменных кончился матч или нет
+			enum WhoseMoving {PLAYER1, PLAYER2}; // задаем массив для хранения переменных кончился чей ход
+			struct MatchInfo
+			{
+				short int FieldSize;
+				char Player1Name[30] = "JohnShepard"; // имя игрока 1, так и не смог нормально ничего записать сюда, наверное надо какую то другую переменную использовать вместо массива char
+				char Player2Name[30] = "JackHarper"; // имя игрока 2, так и не смог нормально ничего записать сюда, наверное надо какую то другую переменную использовать вместо массива char
+				Tic_Tac_Toe Player1Char; // чем ходит игрок 1
+				Tic_Tac_Toe Player2Char; // чем ходит игрок 2
+				WhoseMoving Move; // чей ход
+				GameInProgress Status; // статус матча
+				MatchResult Result; // результат матча
+			};
+
+			MatchInfo Game1;
+			Game1.FieldSize = GameField;
+			Game1.Player1Char = CROSS;
+			Game1.Player2Char = ZERO;
+			Game1.Move = PLAYER2;
+			Game1.Status = PLAYING;
+			Game1.Result = NOTDECIDED;			
+		}
+
+
+
 	}
 
+	{
+		std::cout << "----------Task 5**----------" << std::endl;
+/*
+Попробуем написать свою variant переменную, как это все автоматизировать я пока не знаю,
+поэтому просто запишу и выведу на экран
+*/
+		// записываем структуру юнион где будет храниться наша трехтиповая переменная
+		union VariantType
+		{
+			int Number;
+			float FractNumber;
+			char Symbol;
+		};
 
+		// Объявляем переменную Testvar типа Number входящую в структуру VariantType
+		VariantType Testvar;
+		Testvar.Number = 26;
 
-	
+		// записываем структуру битового флага 
+		struct Flag
+		{
+			unsigned int NumberFlag : 1;
+			unsigned int FractNumberFlag : 1;
+			unsigned int SymbolFlag : 1;
+		};
 
-
-	
-
-
+		// Ставим флаг в переменную NumberFlag
+		Flag TestvarFlag;
+		TestvarFlag.NumberFlag = 1;
+		TestvarFlag.FractNumberFlag = 0;
+		TestvarFlag.SymbolFlag = 0;
+		std::cout << "Lets look where our integer(26) hide: " << std::endl;
+		std::cout << "\tIts our number type slot: " << Testvar.Number << std::endl;
+		std::cout << "\tIts our fractional number type slot: " << Testvar.FractNumber << std::endl;
+		std::cout << "\tIts our symbol type slot: " << Testvar.Symbol << std::endl;
+		std::cout << "Lets look where our flag: " << std::endl;
+		std::cout << "\tIts our number type flag: " << TestvarFlag.NumberFlag << std::endl;
+		std::cout << "\tIts our fractional number type flag: " << TestvarFlag.FractNumberFlag << std::endl;
+		std::cout << "\tIts our symbol type flag: " << TestvarFlag.SymbolFlag << std::endl;
 	}
+
 	return 0;
 }
